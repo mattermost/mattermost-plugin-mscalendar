7f1f90c9b2d7b537bc8309f809ac660dadbc7ab2
Jason Frerich <jason.frerich@mattermost.com>
2019-12-13 12:13:27 -0600

    wip

--- server/remote/msgraph/find_meeting_times.go
+++ server/remote/msgraph/find_meeting_times.go
@@ -4,28 +4,20 @@
 package msgraph
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/mattermost/mattermost-plugin-msoffice/server/remote"
-	msgraph "github.com/yaegashi/msgraph.go/v1.0"
 )
 
 // FindMeetingTimes finds Meetinsa calendar event
 func (c *client) FindMeetingTimes(findMeetingsParam *remote.FindMeetingTimesParameters) (*remote.MeetingTimeSuggestionResults, error) {
 	meetingsOut := &remote.MeetingTimeSuggestionResults{}
-	fakeReq := &msgraph.UserFindMeetingTimesRequestParameter{}
-	fakeRes := &msgraph.MeetingTimeSuggestionsResult{}
 
-	req := c.rbuilder.Me().FindMeetingTimes(fakeReq).Request()
-	err := req.JSONRequest(c.ctx, http.MethodPost, "", &findMeetingsParam, &fakeRes)
+	req := c.rbuilder.Me().FindMeetingTimes(nil).Request()
+	err := req.JSONRequest(c.ctx, http.MethodPost, "", &findMeetingsParam, &meetingsOut)
 
-	fakeReqD, _ := json.MarshalIndent(fakeReq, "", "    ")
-	fmt.Printf("fakeReq = %+v\n", string(fakeReqD))
-
-	fakeResD, _ := json.MarshalIndent(fakeRes, "", "    ")
-	fmt.Printf("fakeRes = %+v\n", string(fakeResD))
+	// fakeResD, _ := json.MarshalIndent(fakeRes, "", "    ")
+	// fmt.Printf("fakeRes = %+v\n", string(fakeResD))
 
 	if err != nil {
 		return nil, err
