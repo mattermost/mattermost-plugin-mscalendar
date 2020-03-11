09a17ab3552a9035ca051aef5305162ebf2505c2
Daniel Espino <larkox@gmail.com>
2020-03-11 08:07:04 -0400

    Handle case where mail is nil

--- server/remote/msgraph/get_me.go
+++ server/remote/msgraph/get_me.go
@@ -3,13 +3,31 @@
 
 package msgraph
 
-import "github.com/mattermost/mattermost-plugin-mscalendar/server/remote"
+import (
+	"fmt"
+
+	"github.com/mattermost/mattermost-plugin-mscalendar/server/remote"
+)
 
 func (c *client) GetMe() (*remote.User, error) {
 	graphUser, err := c.rbuilder.Me().Request().Get(c.ctx)
 	if err != nil {
 		return nil, err
 	}
+
+	if graphUser.ID == nil {
+		return nil, fmt.Errorf("User has no ID")
+	}
+	if graphUser.DisplayName == nil {
+		return nil, fmt.Errorf("User has no Display Name")
+	}
+	if graphUser.UserPrincipalName == nil {
+		return nil, fmt.Errorf("User has no Principal Name")
+	}
+	if graphUser.Mail == nil {
+		return nil, fmt.Errorf("User has no mail")
+	}
+
 	user := &remote.User{
 		ID:                *graphUser.ID,
 		DisplayName:       *graphUser.DisplayName,
