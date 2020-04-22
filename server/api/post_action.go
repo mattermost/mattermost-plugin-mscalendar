6039ba65f61844e5cca59e33c476a54cc7129c7c
Daniel Espino García <larkox@gmail.com>
2020-04-22 14:32:13 +0200

    [GH-53] Accept or decline meetings from the notification message (#79)
    * Ignore events created by the user
    
    * Remove actions when you are the organizer and update post after response
    
    * Just notify on important changes
    
    * Address suggestions
    
    * Improve error handling on slack attachments
    
    * Use notification UID in the store, and remove the option to respond to an event after responding with post action
    
    * Fix nil reference panics and response to cancelled events.
    
    Co-authored-by: mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Lev <1187448+levb@users.noreply.github.com>

--- server/api/post_action.go
+++ server/api/post_action.go
@@ -4,80 +4,133 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/mattermost/mattermost-server/v5/model"
 
 	"github.com/mattermost/mattermost-plugin-mscalendar/server/config"
 	"github.com/mattermost/mattermost-plugin-mscalendar/server/mscalendar"
+	"github.com/mattermost/mattermost-plugin-mscalendar/server/utils"
 )
 
-func (api *api) preprocessAction(w http.ResponseWriter, req *http.Request) (mscalendar.MSCalendar, *mscalendar.User, string, string) {
+func (api *api) preprocessAction(w http.ResponseWriter, req *http.Request) (mscal mscalendar.MSCalendar, user *mscalendar.User, eventID string, option string, postID string) {
 	mattermostUserID := req.Header.Get("Mattermost-User-ID")
+	if mattermostUserID == "" {
+		utils.SlackAttachmentError(w, "Error: not authorized")
+		return nil, nil, "", "", ""
+	}
 
 	request := model.PostActionIntegrationRequestFromJson(req.Body)
 	if request == nil {
-		http.Error(w, "invalid request", http.StatusBadRequest)
-		return nil, nil, "", ""
+		utils.SlackAttachmentError(w, "Error: invalid request")
+		return nil, nil, "", "", ""
 	}
 
 	eventID, ok := request.Context[config.EventIDKey].(string)
 	if !ok {
-		http.Error(w, "invalid request", http.StatusBadRequest)
-		return nil, nil, "", ""
+		utils.SlackAttachmentError(w, "Error: missing event ID")
+		return nil, nil, "", "", ""
 	}
-	option, _ := request.Context["selected_option"].(string)
-	mscal := mscalendar.New(api.Env, mattermostUserID)
+	option, _ = request.Context["selected_option"].(string)
+	mscal = mscalendar.New(api.Env, mattermostUserID)
 
-	return mscal, mscalendar.NewUser(mattermostUserID), eventID, option
+	return mscal, mscalendar.NewUser(mattermostUserID), eventID, option, request.PostId
 }
 
 func (api *api) postActionAccept(w http.ResponseWriter, req *http.Request) {
-	mscalendar, user, eventID, _ := api.preprocessAction(w, req)
+	mscalendar, user, eventID, _, _ := api.preprocessAction(w, req)
 	if eventID == "" {
 		return
 	}
 	err := mscalendar.AcceptEvent(user, eventID)
 	if err != nil {
 		api.Logger.Warnf(err.Error())
-		http.Error(w, "Failed to accept event: "+err.Error(), http.StatusBadRequest)
+		utils.SlackAttachmentError(w, "Error: Failed to accept event: "+err.Error())
 		return
 	}
 }
 
 func (api *api) postActionDecline(w http.ResponseWriter, req *http.Request) {
-	mscalendar, user, eventID, _ := api.preprocessAction(w, req)
+	mscalendar, user, eventID, _, _ := api.preprocessAction(w, req)
 	if eventID == "" {
 		return
 	}
 	err := mscalendar.DeclineEvent(user, eventID)
 	if err != nil {
-		http.Error(w, "Failed to decline event: "+err.Error(), http.StatusBadRequest)
+		utils.SlackAttachmentError(w, "Error: Failed to decline event: "+err.Error())
 		return
 	}
 }
 
 func (api *api) postActionTentative(w http.ResponseWriter, req *http.Request) {
-	mscalendar, user, eventID, _ := api.preprocessAction(w, req)
+	mscalendar, user, eventID, _, _ := api.preprocessAction(w, req)
 	if eventID == "" {
 		return
 	}
 	err := mscalendar.TentativelyAcceptEvent(user, eventID)
 	if err != nil {
-		http.Error(w, "Failed to tentatively accept event: "+err.Error(), http.StatusBadRequest)
+		utils.SlackAttachmentError(w, "Error: Failed to tentatively accept event: "+err.Error())
 		return
 	}
 }
 
 func (api *api) postActionRespond(w http.ResponseWriter, req *http.Request) {
-	mscalendar, user, eventID, option := api.preprocessAction(w, req)
+	calendar, user, eventID, option, postID := api.preprocessAction(w, req)
 	if eventID == "" {
 		return
 	}
-	err := mscalendar.RespondToEvent(user, eventID, option)
-	if err != nil {
-		// h.LogWarn(err.Error())
-		http.Error(w, "Failed to respond to event: "+err.Error(), http.StatusBadRequest)
+	err := calendar.RespondToEvent(user, eventID, option)
+	if err != nil && !strings.HasPrefix(err.Error(), "202") && !strings.HasPrefix(err.Error(), "404") {
+		utils.SlackAttachmentError(w, "Error: Failed to respond to event: "+err.Error())
 		return
+	}
+
+	p, err := api.PluginAPI.GetPost(postID)
+	if err != nil {
+		utils.SlackAttachmentError(w, "Error: Failed to update the post: "+err.Error())
+		return
+	}
+
+	sas := p.Attachments()
+	if len(sas) == 0 {
+		utils.SlackAttachmentError(w, "Error: Failed to update the post: "+err.Error())
+		return
+	}
+
+	sa := sas[0]
+
+	if err == nil || strings.HasPrefix(err.Error(), "202") {
+		sa.Fields = append(sa.Fields, &model.SlackAttachmentField{
+			Title: "Response",
+			Value: fmt.Sprintf("You have %s this event", prettyOption(option)),
+			Short: false,
+		})
+	}
+
+	sa.Actions = []*model.PostAction{}
+	postResponse := model.PostActionIntegrationResponse{}
+	model.ParseSlackAttachment(p, []*model.SlackAttachment{sa})
+
+	postResponse.Update = p
+
+	if err != nil && strings.HasPrefix(err.Error(), "404") {
+		postResponse.EphemeralText = "Event has changed since this message. Please change your status directly on MS Calendar."
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(postResponse.ToJson())
+}
+
+func prettyOption(option string) string {
+	switch option {
+	case mscalendar.OptionYes:
+		return "accepted"
+	case mscalendar.OptionNo:
+		return "declined"
+	case mscalendar.OptionMaybe:
+		return "tentatively accepted"
+	default:
+		return ""
 	}
 }
