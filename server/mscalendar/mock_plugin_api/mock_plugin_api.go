09f5a92ac70dc617160f6e9843df37bc92773bfc
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2021-09-09 14:18:01 -0500

    Revert "Implement autorespond feature (#188)"
    This reverts commit b8aff5a675f4ddd042569bda9ed9316be19cefed.

--- server/mscalendar/mock_plugin_api/mock_plugin_api.go
+++ server/mscalendar/mock_plugin_api/mock_plugin_api.go
@@ -33,21 +33,6 @@ func (m *MockPluginAPI) EXPECT() *MockPluginAPIMockRecorder {
 	return m.recorder
 }
 
-// GetMattermostChannel mocks base method
-func (m *MockPluginAPI) GetMattermostChannel(arg0 string) (*model.Channel, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "GetMattermostChannel", arg0)
-	ret0, _ := ret[0].(*model.Channel)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// GetMattermostChannel indicates an expected call of GetMattermostChannel
-func (mr *MockPluginAPIMockRecorder) GetMattermostChannel(arg0 interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetMattermostChannel", reflect.TypeOf((*MockPluginAPI)(nil).GetMattermostChannel), arg0)
-}
-
 // GetMattermostUser mocks base method
 func (m *MockPluginAPI) GetMattermostUser(arg0 string) (*model.User, error) {
 	m.ctrl.T.Helper()
@@ -78,21 +63,6 @@ func (mr *MockPluginAPIMockRecorder) GetMattermostUserByUsername(arg0 interface{
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetMattermostUserByUsername", reflect.TypeOf((*MockPluginAPI)(nil).GetMattermostUserByUsername), arg0)
 }
 
-// GetMattermostUserStatus mocks base method
-func (m *MockPluginAPI) GetMattermostUserStatus(arg0 string) (*model.Status, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "GetMattermostUserStatus", arg0)
-	ret0, _ := ret[0].(*model.Status)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// GetMattermostUserStatus indicates an expected call of GetMattermostUserStatus
-func (mr *MockPluginAPIMockRecorder) GetMattermostUserStatus(arg0 interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetMattermostUserStatus", reflect.TypeOf((*MockPluginAPI)(nil).GetMattermostUserStatus), arg0)
-}
-
 // GetMattermostUserStatusesByIds mocks base method
 func (m *MockPluginAPI) GetMattermostUserStatusesByIds(arg0 []string) ([]*model.Status, error) {
 	m.ctrl.T.Helper()
@@ -106,21 +76,6 @@ func (m *MockPluginAPI) GetMattermostUserStatusesByIds(arg0 []string) ([]*model.
 func (mr *MockPluginAPIMockRecorder) GetMattermostUserStatusesByIds(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetMattermostUserStatusesByIds", reflect.TypeOf((*MockPluginAPI)(nil).GetMattermostUserStatusesByIds), arg0)
-}
-
-// GetMattermostUsersInChannel mocks base method
-func (m *MockPluginAPI) GetMattermostUsersInChannel(arg0, arg1 string, arg2, arg3 int) ([]*model.User, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "GetMattermostUsersInChannel", arg0, arg1, arg2, arg3)
-	ret0, _ := ret[0].([]*model.User)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// GetMattermostUsersInChannel indicates an expected call of GetMattermostUsersInChannel
-func (mr *MockPluginAPIMockRecorder) GetMattermostUsersInChannel(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetMattermostUsersInChannel", reflect.TypeOf((*MockPluginAPI)(nil).GetMattermostUsersInChannel), arg0, arg1, arg2, arg3)
 }
 
 // GetPost mocks base method
@@ -151,20 +106,6 @@ func (m *MockPluginAPI) IsSysAdmin(arg0 string) (bool, error) {
 func (mr *MockPluginAPIMockRecorder) IsSysAdmin(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "IsSysAdmin", reflect.TypeOf((*MockPluginAPI)(nil).IsSysAdmin), arg0)
-}
-
-// OpenInteractiveDialog mocks base method
-func (m *MockPluginAPI) OpenInteractiveDialog(arg0 model.OpenDialogRequest) error {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "OpenInteractiveDialog", arg0)
-	ret0, _ := ret[0].(error)
-	return ret0
-}
-
-// OpenInteractiveDialog indicates an expected call of OpenInteractiveDialog
-func (mr *MockPluginAPIMockRecorder) OpenInteractiveDialog(arg0 interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "OpenInteractiveDialog", reflect.TypeOf((*MockPluginAPI)(nil).OpenInteractiveDialog), arg0)
 }
 
 // UpdateMattermostUserStatus mocks base method
