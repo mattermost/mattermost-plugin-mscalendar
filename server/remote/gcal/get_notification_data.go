734e3d68466c85310475a58c0390af81f6340a70
Felipe Martin <me@fmartingr.com>
2023-08-03 13:13:16 +0200

    lint

--- server/remote/gcal/get_notification_data.go
+++ server/remote/gcal/get_notification_data.go
@@ -4,12 +4,7 @@
 package gcal
 
 import (
-	"context"
 	"fmt"
-
-	"github.com/pkg/errors"
-	"google.golang.org/api/calendar/v3"
-	"google.golang.org/api/option"
 
 	"github.com/mattermost/mattermost-plugin-mscalendar/server/remote"
 )
@@ -17,29 +12,29 @@ import (
 func (c *client) GetNotificationData(orig *remote.Notification) (*remote.Notification, error) {
 	return nil, fmt.Errorf("not implemented")
 
-	service, err := calendar.NewService(context.Background(), option.WithHTTPClient(c.httpClient))
-	if err != nil {
-		return nil, errors.Wrap(err, "gcal GetNotificationData, error creating service")
-	}
+	// service, err := calendar.NewService(context.Background(), option.WithHTTPClient(c.httpClient))
+	// if err != nil {
+	// 	return nil, errors.Wrap(err, "gcal GetNotificationData, error creating service")
+	// }
 
-	n := *orig
-	wh := n.Webhook.(*webhook)
+	// n := *orig
+	// wh := n.Webhook.(*webhook)
 
-	cal, err := c.GetDefaultCalendar()
-	if err != nil {
-		return nil, errors.Wrap(err, "gcal GetNotificationData, error getting default calendar")
-	}
+	// cal, err := c.GetDefaultCalendar()
+	// if err != nil {
+	// 	return nil, errors.Wrap(err, "gcal GetNotificationData, error getting default calendar")
+	// }
 
-	reqBody := service.Events.Get(cal.ID, wh.Resource)
-	googleEvent, err := reqBody.Do()
-	if err != nil {
-		return nil, errors.Wrap(err, "gcal GetNotificationData, error fetching event data")
-	}
+	// reqBody := service.Events.Get(cal.ID, wh.Resource)
+	// googleEvent, err := reqBody.Do()
+	// if err != nil {
+	// 	return nil, errors.Wrap(err, "gcal GetNotificationData, error fetching event data")
+	// }
 
-	event := convertGCalEventToRemoteEvent(googleEvent)
+	// event := convertGCalEventToRemoteEvent(googleEvent)
 
-	n.Event = event
-	n.IsBare = false
+	// n.Event = event
+	// n.IsBare = false
 
-	return &n, nil
+	// return &n, nil
 }
