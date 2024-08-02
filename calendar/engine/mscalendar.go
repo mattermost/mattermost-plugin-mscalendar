5b5b7e91cd06cd3a301e34f563e1f7a0d5031ba7
THEBOSS0369 <anujkumsharma9876@gmail.com>
2024-08-02 10:52:15 +0530

    support different time format

--- calendar/engine/mscalendar.go
+++ calendar/engine/mscalendar.go
@@ -4,16 +4,25 @@
 package engine
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/remote"
 	"github.com/mattermost/mattermost/server/public/model"
 
 	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/config"
-	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/remote"
 	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/store"
 	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/tracker"
 	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/utils/bot"
 	"github.com/mattermost/mattermost-plugin-mscalendar/calendar/utils/settingspanel"
 )
 
+// Assuming Plugin type is properly defined in another file
+type Plugin struct {
+	API PluginAPI
+}
+
+// Interface definitions
 type Engine interface {
 	Availability
 	Calendar
@@ -50,6 +59,8 @@ type PluginAPI interface {
 	SearchLinkableChannelForUser(teamID, mattemostUserID, search string) ([]*model.Channel, error)
 	GetMattermostUserTeams(mattermostUserID string) ([]*model.Team, error)
 	PublishWebsocketEvent(mattermostUserID, event string, payload map[string]any)
+	GetUserPreferences(userID string) ([]*model.Preference, error)          // Added method for retrieving user preferences
+	SendEphemeralPost(userID string, post *model.Post) (*model.Post, error) // Ensure this method exists
 }
 
 type Env struct {
@@ -62,16 +73,18 @@ type mscalendar struct {
 
 	actingUser *User
 	client     remote.Client
+	plugin     *Plugin
 }
 
 // copy returns a copy of the calendar engine
-func (m mscalendar) copy() *mscalendar {
+func (m *mscalendar) copy() *mscalendar {
 	user := *m.actingUser
 	client := m.client
 	return &mscalendar{
 		Env:        m.Env,
 		actingUser: &user,
 		client:     client,
+		plugin:     m.plugin,
 	}
 }
 
@@ -79,5 +92,72 @@ func New(env Env, actingMattermostUserID string) Engine {
 	return &mscalendar{
 		Env:        env,
 		actingUser: NewUser(actingMattermostUserID),
+		plugin:     &Plugin{API: env.PluginAPI},
 	}
+}
+
+func formatTime(t time.Time, format string) string {
+	if format == "24-hour" {
+		return t.Format("15:04")
+	}
+	return t.Format("03:04 PM")
+}
+
+func (m *mscalendar) getUserTimeFormat(userID string) (string, error) {
+	user, err := m.plugin.API.GetMattermostUser(userID)
+	if err != nil {
+		return "", err
+	}
+	fmt.Println("User: ", user)
+
+	// Replace with actual implementation to get user preferences
+	preferences, err := m.plugin.API.GetUserPreferences(userID)
+	if err != nil {
+		return "", err
+	}
+
+	timeFormat := "12-hour" // Default value
+	for _, pref := range preferences {
+		if pref.Category == model.PreferenceCategoryDisplaySettings && pref.Name == model.PreferenceNameUseMilitaryTime {
+			if pref.Value == "true" {
+				timeFormat = "24-hour"
+			}
+			break
+		}
+	}
+
+	return timeFormat, nil
+}
+
+func convertDateTime(dt *remote.DateTime) time.Time {
+	// Implement this function based on the actual structure of remote.DateTime
+	return time.Time{} // Placeholder
+}
+
+func (m *mscalendar) PostEventToUser(userID string, event *remote.Event) error {
+	timeFormat, err := m.getUserTimeFormat(userID)
+	if err != nil {
+		return err
+	}
+
+	// Convert remote.DateTime to time.Time
+	startTime := convertDateTime(event.Start)
+	endTime := convertDateTime(event.End)
+
+	// Using the timeFormat to format the event's time before posting it
+	formattedStartTime := formatTime(startTime, timeFormat)
+	formattedEndTime := formatTime(endTime, timeFormat)
+
+	// Ensure that event.Title is available in remote.Event or use a default
+	title := "Unknown Event Title" // Placeholder
+	if event != nil && event.Subject != "" {
+		title = event.Subject
+	}
+
+	// Posting the event to the user
+	message := fmt.Sprintf("Event: %s from %s to %s", title, formattedStartTime, formattedEndTime)
+	_, err = m.plugin.API.SendEphemeralPost(userID, &model.Post{
+		Message: message,
+	})
+	return err
 }
--- calendar/remote/client.go
+++ calendar/remote/client.go
@@ -6,6 +6,8 @@ package remote
 import (
 	"net/url"
 	"time"
+
+	"golang.org/x/oauth2"
 )
 
 type Client interface {
@@ -19,6 +21,125 @@ type Client interface {
 
 type Core interface {
 	GetMe() (*User, error)
+}
+
+type myClientImplementation struct {
+	token *oauth2.Token
+}
+
+// AcceptEvent implements Client.
+func (c *myClientImplementation) AcceptEvent(remoteUserID string, eventID string) error {
+	panic("unimplemented")
+}
+
+// CallFormPost implements Client.
+func (c *myClientImplementation) CallFormPost(method string, path string, in url.Values, out interface{}) (responseData []byte, err error) {
+	panic("unimplemented")
+}
+
+// CallJSON implements Client.
+func (c *myClientImplementation) CallJSON(method string, path string, in interface{}, out interface{}) (responseData []byte, err error) {
+	panic("unimplemented")
+}
+
+// CreateCalendar implements Client.
+func (c *myClientImplementation) CreateCalendar(remoteUserID string, calendar *Calendar) (*Calendar, error) {
+	panic("unimplemented")
+}
+
+// CreateEvent implements Client.
+func (c *myClientImplementation) CreateEvent(remoteUserID string, calendarEvent *Event) (*Event, error) {
+	panic("unimplemented")
+}
+
+// CreateMySubscription implements Client.
+func (c *myClientImplementation) CreateMySubscription(notificationURL string, remoteUserID string) (*Subscription, error) {
+	panic("unimplemented")
+}
+
+// DeclineEvent implements Client.
+func (c *myClientImplementation) DeclineEvent(remoteUserID string, eventID string) error {
+	panic("unimplemented")
+}
+
+// DeleteCalendar implements Client.
+func (c *myClientImplementation) DeleteCalendar(remoteUserID string, calendarID string) error {
+	panic("unimplemented")
+}
+
+// DeleteSubscription implements Client.
+func (c *myClientImplementation) DeleteSubscription(sub *Subscription) error {
+	panic("unimplemented")
+}
+
+// DoBatchViewCalendarRequests implements Client.
+func (c *myClientImplementation) DoBatchViewCalendarRequests([]*ViewCalendarParams) ([]*ViewCalendarResponse, error) {
+	panic("unimplemented")
+}
+
+// FindMeetingTimes implements Client.
+func (c *myClientImplementation) FindMeetingTimes(remoteUserID string, meetingParams *FindMeetingTimesParameters) (*MeetingTimeSuggestionResults, error) {
+	panic("unimplemented")
+}
+
+// GetCalendars implements Client.
+func (c *myClientImplementation) GetCalendars(remoteUserID string) ([]*Calendar, error) {
+	panic("unimplemented")
+}
+
+// GetDefaultCalendarView implements Client.
+func (c *myClientImplementation) GetDefaultCalendarView(remoteUserID string, startTime time.Time, endTime time.Time) ([]*Event, error) {
+	panic("unimplemented")
+}
+
+// GetEvent implements Client.
+func (c *myClientImplementation) GetEvent(remoteUserID string, eventID string) (*Event, error) {
+	panic("unimplemented")
+}
+
+// GetEventsBetweenDates implements Client.
+func (c *myClientImplementation) GetEventsBetweenDates(remoteUserID string, start time.Time, end time.Time) ([]*Event, error) {
+	panic("unimplemented")
+}
+
+// GetMailboxSettings implements Client.
+func (c *myClientImplementation) GetMailboxSettings(remoteUserID string) (*MailboxSettings, error) {
+	panic("unimplemented")
+}
+
+// GetNotificationData implements Client.
+func (c *myClientImplementation) GetNotificationData(*Notification) (*Notification, error) {
+	panic("unimplemented")
+}
+
+// GetSuperuserToken implements Client.
+func (c *myClientImplementation) GetSuperuserToken() (string, error) {
+	panic("unimplemented")
+}
+
+// ListSubscriptions implements Client.
+func (c *myClientImplementation) ListSubscriptions() ([]*Subscription, error) {
+	panic("unimplemented")
+}
+
+// RenewSubscription implements Client.
+func (c *myClientImplementation) RenewSubscription(notificationURL string, remoteUserID string, sub *Subscription) (*Subscription, error) {
+	panic("unimplemented")
+}
+
+// TentativelyAcceptEvent implements Client.
+func (c *myClientImplementation) TentativelyAcceptEvent(remoteUserID string, eventID string) error {
+	panic("unimplemented")
+}
+
+func NewClient(token *oauth2.Token) Client {
+	return &myClientImplementation{token: token}
+}
+
+// Implement Client interface methods
+func (c *myClientImplementation) GetMe() (*User, error) {
+	// Example implementation
+	return nil, nil
 }
 
 type Calendars interface {
