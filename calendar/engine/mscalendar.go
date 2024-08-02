2273d78e54c5d9eb692167808051982c9c93c70b
Lorenzo Gallegos <1328683+enzowritescode@users.noreply.github.com>
2024-08-02 15:08:28 -0600

    Fix typo

--- calendar/engine/mscalendar.go
+++ calendar/engine/mscalendar.go
@@ -47,7 +47,7 @@ type PluginAPI interface {
 	UpdateMattermostUserStatus(mattermostUserID, status string) (*model.Status, error)
 	GetPost(postID string) (*model.Post, error)
 	CanLinkEventToChannel(channelID, userID string) bool
-	SearchLinkableChannelForUser(teamID, mattemostUserID, search string) ([]*model.Channel, error)
+	SearchLinkableChannelForUser(teamID, mattermostUserID, search string) ([]*model.Channel, error)
 	GetMattermostUserTeams(mattermostUserID string) ([]*model.Team, error)
 	PublishWebsocketEvent(mattermostUserID, event string, payload map[string]any)
 }
