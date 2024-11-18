2a8a9385db0f968d20c8532d5a91d6fbd02dafae
kshitij katiyar <90389917+Kshitij-Katiyar@users.noreply.github.com>
2024-11-18 16:18:13 +0530

    Added server testcases for store/oauth2_store.go

--- calendar/store/test_util.go
+++ calendar/store/test_util.go
@@ -30,6 +30,7 @@ const (
 	MockRemoteJSON               = `{"remote": {"id": "mockRemoteID"}}`
 	MockUserJSON                 = `[{"MattermostUserID":"mockMMUserID","RemoteID":"mockRemoteID"}]`
 	MockUserDetailsWithEventJSON = `{"mm_id":"mockUserID","active_events": []}`
+	MockState                    = "mockState"
 )
 
 func GetMockSetup(t *testing.T) (*testutil.MockPluginAPI, Store, *mock_bot.MockLogger, *mock_bot.MockLogger, *mock_tracker.MockTracker) {
